8af5d4a9cb4468dc83e771ad21590641fc111d9e
bdrachovska <44036264+bdrachovska@users.noreply.github.com>
2022-04-14 23:43:47 +0200

    oprava className

--- src/components/Energy/index.jsx
+++ src/components/Energy/index.jsx
@@ -5,23 +5,23 @@ import React from 'react';
 const Energy= ({electricity, water}) => {
 
 	return (
-<div class="energy">
-				<div class="energy__source">
-					<div class="energy__icon">
+<div className="energy">
+				<div className="energy__source">
+					<div className="energy__icon">
 						<img src="./images/electricity.svg"/>
 					</div>
-					<div class="energy__consumption">
-						<div class="energy__description">Elektřina</div>
-						<div class="energy__value">{electricity} kW</div>
+					<div className="energy__consumption">
+						<div className="energy__description">Elektřina</div>
+						<div className="energy__value">{electricity} kW</div>
 					</div>
 				</div>
-				<div class="energy__source">
-					<div class="energy__icon">
+				<div className="energy__source">
+					<div className="energy__icon">
 						<img src="./images/water.svg"/>
 					</div>
-					<div class="energy__consumption">
-						<div class="energy__description">Voda</div>
-						<div class="energy__value">{water} m<sup>3</sup></div>
+					<div className="energy__consumption">
+						<div className="energy__description">Voda</div>
+						<div className="energy__value">{water} m<sup>3</sup></div>
 					</div>
 				</div>
                 </div>
